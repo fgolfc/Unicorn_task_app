6cf5769a04bbabf8c6b1aba20e481989ebccc970
fgolfc <cf.ts.fujimoto@gmail.com>
2023-05-30 15:59:07 +0900

    add s3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10"
 
+set :linked_files, %w{config/secrets.yml .env}
 set :application, "task_app"
 set :repo_url, "https://github.com/fgolfc/Unicorn_task_app.git"
 set :linked_files, %w{config/secrets.yml}   # 4
